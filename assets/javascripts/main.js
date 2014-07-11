65ef1007c135c6e9ca5a11f6086f55fda3cef38d
mfranczyk <mfranczyk@wip.pl>
2014-07-11 16:10:14 +0200

    new pages + sass
    + listpage
    + static-page

--- assets/javascripts/main.js
+++ assets/javascripts/main.js
@@ -1,4 +1,38 @@
 jQuery(document).ready(function ($) {
+
+    //"Top" button
+    var scroll_timer;
+    var displayed = false;
+    var $message = jQuery('#message-footer');
+    var $window = jQuery(window);
+    var top = jQuery(document.body).children(0).position().top;
+    
+    $window.scroll(function () {
+        window.clearTimeout(scroll_timer);
+        scroll_timer = window.setTimeout(function () { 
+        if($window.scrollTop() <= top) 
+        {
+            displayed = false;
+            $message.fadeOut(500);
+        }
+        else if(displayed == false) 
+        {
+            displayed = true;
+            $message.stop(true, true).fadeIn(500).click(function () { $message.fadeOut(500); });
+        }
+        }, 400);
+    });
+    
+    jQuery('#top-link').click(function(e) {
+            jQuery('html, body').animate({scrollTop:0}, 'slow');
+            return false;
+    });
+
+    jQuery('#nav-lupa a').click(function(){
+        jQuery(this).parent().children('form').toggle();
+    });
+
+
     var options = {
         $AutoPlay: false,
 
@@ -63,37 +97,7 @@ jQuery(document).ready(function ($) {
 
 
 
-    //"Top" button
-    var scroll_timer;
-    var displayed = false;
-    var $message = jQuery('#message-footer');
-    var $window = jQuery(window);
-    var top = jQuery(document.body).children(0).position().top;
     
-    $window.scroll(function () {
-        window.clearTimeout(scroll_timer);
-        scroll_timer = window.setTimeout(function () { 
-        if($window.scrollTop() <= top) 
-        {
-            displayed = false;
-            $message.fadeOut(500);
-        }
-        else if(displayed == false) 
-        {
-            displayed = true;
-            $message.stop(true, true).fadeIn(500).click(function () { $message.fadeOut(500); });
-        }
-        }, 400);
-    });
-    
-    jQuery('#top-link').click(function(e) {
-            jQuery('html, body').animate({scrollTop:0}, 'slow');
-            return false;
-    });
-
-    jQuery('#nav-lupa a').click(function(){
-        jQuery(this).parent().children('form').toggle();
-    });
 
 
 
